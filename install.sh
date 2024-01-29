5cf2347b6be9c27d4ca86da63b9dace122be8084
Crypto Robot <88462650+CryptoRobotFr@users.noreply.github.com>
2024-01-29 08:12:58 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ touch cronlog.log
 
 echo Installation des packages nécessaires...
 cd Live-Tools-V2
+sudo apt-get install python3-venv
+python3 -m venv .venv
+source .venv/bin/activate
 pip install -r requirements.txt
 git update-index --assume-unchanged secret.py
 cd ..
